b5901eb5fbd8bd47f62239de39ee3c7430b8b03e
Saad <gnkhatri936@gmail.com>
2025-06-23 16:29:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("Error! please enter A valid password");
+alert("Welcome to JS Land...\nHappy Codding!");
+alert("Welcome to JS Land...");
+alert("Happy Codding! \n Prevent this page from creating additional dialogs.");
+alert("Hello... I can run Js through my web browsers console");
